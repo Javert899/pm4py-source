96990fb55785d97870c209352f4e46c93a2fc031
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-22 10:27:41 +0100

    Providing start and end activities directly

--- pm4py/algo/starstar/discovery/versions/classic.py
+++ pm4py/algo/starstar/discovery/versions/classic.py
@@ -1,6 +1,9 @@
 from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
+from pm4py.algo.filtering.pandas.start_activities import start_activities_filter
+from pm4py.algo.filtering.pandas.end_activities import end_activities_filter
 from pm4py.objects.heuristics_net.net import HeuristicsNet
-
+from copy import copy
+from pm4py.util import constants
 
 def apply(df, parameters=None):
     """
@@ -23,7 +26,6 @@ def apply(df, parameters=None):
         parameters = {}
 
     perspectives_heu = {}
-
     perspectives = list(df.columns)
     del perspectives[perspectives.index("event_id")]
     del perspectives[perspectives.index("event_activity")]
@@ -34,8 +36,16 @@ def apply(df, parameters=None):
             proj_df = df[["event_id", "event_activity", p]].dropna()
             dfg_frequency = df_statistics.get_dfg_graph(proj_df, activity_key="event_activity", case_id_glue=p,
                                                         sort_timestamp_along_case_id=False)
+
             if len(dfg_frequency) > 0:
-                heu_net = HeuristicsNet(dfg_frequency)
+                parameters_sa_ea = copy(parameters)
+                parameters_sa_ea[constants.PARAMETER_CONSTANT_CASEID_KEY] = p
+                parameters_sa_ea[constants.PARAMETER_CONSTANT_ATTRIBUTE_KEY] = "event_activity"
+                parameters_sa_ea[constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = "event_activity"
+                start_activities = start_activities_filter.get_start_activities(proj_df, parameters=parameters_sa_ea)
+                end_activities = end_activities_filter.get_end_activities(proj_df, parameters=parameters_sa_ea)
+
+                heu_net = HeuristicsNet(dfg_frequency, start_activities=start_activities, end_activities=end_activities)
                 heu_net.calculate()
                 if len(heu_net.nodes) > 0:
                     perspectives_heu[p] = heu_net
