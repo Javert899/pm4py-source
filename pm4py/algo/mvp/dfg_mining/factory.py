98f3d25135032fe06d282abb2dffa0392cb1f3a2
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-03 08:24:07 +0200

    Added DFG mining from discovered MVP

---
+++ pm4py/algo/mvp/dfg_mining/factory.py
@@ -0,0 +1,30 @@
+from pm4py.algo.mvp.dfg_mining.versions import classic
+
+CLASSIC = "classic"
+
+VERSIONS = {CLASSIC: classic.apply}
+
+
+def apply(mvp, variant=CLASSIC, parameters=None):
+    """
+    Apply DFG mining
+
+    Parameters
+    ------------
+    mvp
+        Multiple Viewpoint Model
+    variant
+        Variant of the algorithm to apply, possible values: classic
+    parameters
+        Parameters of the algorithm
+
+    Returns
+    ------------
+    net
+        Petri net
+    im
+        Initial marking
+    fm
+        Final marking
+    """
+    return classic.apply(mvp, parameters=parameters)
---
+++ pm4py/algo/mvp/dfg_mining/versions/classic.py
@@ -0,0 +1,94 @@
+from pm4py.objects.petri.petrinet import PetriNet, Marking
+from pm4py.objects.petri.utils import add_arc_from_to
+
+
+def apply(mvp, parameters=None):
+    """
+    Apply DFG mining
+
+    Parameters
+    ------------
+    mvp
+        Multiple Viewpoint Model
+    parameters
+        Parameters of the algorithm
+
+    Returns
+    ------------
+    net
+        Petri net
+    im
+        Initial marking
+    fm
+        Final marking
+    """
+    if parameters is None:
+        parameters = {}
+
+    perspectives = parameters["perspectives"]
+
+    net = PetriNet("")
+    im = Marking()
+    fm = Marking()
+
+    corr_places = {}
+
+    master_source = PetriNet.Place("master_source")
+    net.places.add(master_source)
+    im[master_source] = 1
+    master_so_ht = PetriNet.Transition("master_so_ht")
+    net.transitions.add(master_so_ht)
+    add_arc_from_to(master_source, master_so_ht, net)
+    master_sink = PetriNet.Place("master_sink")
+    fm[master_sink] = 1
+    net.places.add(master_sink)
+    master_si_ht = PetriNet.Transition("master_si_ht")
+    net.transitions.add(master_si_ht)
+    add_arc_from_to(master_si_ht, master_sink, net)
+
+    for index, perspective in enumerate(perspectives):
+        start_activities = list(mvp[perspective].start_activities[0].keys())
+        end_activites = list(mvp[perspective].end_activities[0].keys())
+        nodes = mvp[perspective].nodes
+
+        source = PetriNet.Place("so_" + perspective)
+        net.places.add(source)
+        ht = PetriNet.Transition("htso_"+perspective)
+        net.transitions.add(ht)
+        add_arc_from_to(master_so_ht, source, net)
+        add_arc_from_to(source, ht, net)
+
+        for act in start_activities:
+            if act not in corr_places:
+                corr_places[act] = PetriNet.Place("p_"+act)
+                net.places.add(corr_places[act])
+            add_arc_from_to(ht, corr_places[act], net)
+
+        sink = PetriNet.Place("si_" + perspective)
+        net.places.add(sink)
+        ht = PetriNet.Transition("htsi_"+perspective)
+        net.transitions.add(ht)
+        add_arc_from_to(ht, sink, net)
+        add_arc_from_to(sink, master_si_ht, net)
+
+        for act in end_activites:
+            if act not in corr_places:
+                corr_places[act] = PetriNet.Place("p_"+act)
+                net.places.add(corr_places[act])
+            add_arc_from_to(corr_places[act], ht, net)
+
+        for i1, n1 in enumerate(nodes):
+            if n1 not in corr_places:
+                corr_places[n1] = PetriNet.Place("p1_"+n1)
+                net.places.add(corr_places[n1])
+            for i2, n2n in enumerate(nodes[n1].output_connections):
+                n2 = n2n.node_name
+                if n2 not in corr_places:
+                    corr_places[n2] = PetriNet.Place("p2_"+n2)
+                    net.places.add(corr_places[n2])
+                trans = PetriNet.Transition("tr_"+str(index)+"_"+str(i1)+"_"+str(i2), n1)
+                net.transitions.add(trans)
+                add_arc_from_to(corr_places[n1], trans, net)
+                add_arc_from_to(trans, corr_places[n2], net)
+
+    return net, im, fm
