b08c4224a6a0a7800101b2be0878898d22772c5d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-03 13:43:46 +0200

    Fix

--- pm4py/algo/mvp/dfg_mining/versions/classic.py
+++ pm4py/algo/mvp/dfg_mining/versions/classic.py
@@ -49,52 +49,80 @@ def apply(mvp, parameters=None):
 
     for index, perspective in enumerate(perspectives):
         start_activities = list(mvp[perspective].start_activities[0].keys())
-        end_activites = list(mvp[perspective].end_activities[0].keys())
+        end_activities = list(mvp[perspective].end_activities[0].keys())
         nodes = mvp[perspective].nodes
 
         source = PetriNet.Place("so_" + perspective)
         net.places.add(source)
-        ht_source = PetriNet.Transition("htso_"+perspective)
-        net.transitions.add(ht_source)
         add_arc_from_to(master_so_ht, source, net)
-        add_arc_from_to(source, ht_source, net)
 
-        for act in start_activities:
-            if act not in corr_places:
-                corr_places[act] = PetriNet.Place("p_"+act)
-                net.places.add(corr_places[act])
-            add_arc_from_to(ht_source, corr_places[act], net)
+        for index2, act in enumerate(start_activities):
+            ht_source = PetriNet.Transition("htso_" + perspective + "_" + str(index2))
+            net.transitions.add(ht_source)
+            add_arc_from_to(source, ht_source, net)
+
+            #pact = perspective + "@@" + act
+            pact = act
+            if pact not in corr_places:
+                corr_places[pact] = PetriNet.Place("p_"+pact)
+                net.places.add(corr_places[pact])
+
+            add_arc_from_to(ht_source, corr_places[pact], net)
 
         sink = PetriNet.Place("si_" + perspective)
         net.places.add(sink)
-        ht_sink = PetriNet.Transition("htsi_"+perspective)
-        net.transitions.add(ht_sink)
-        add_arc_from_to(ht_sink, sink, net)
         add_arc_from_to(sink, master_si_ht, net)
 
-        for act in end_activites:
-            if act not in corr_places:
-                corr_places[act] = PetriNet.Place("p_"+act)
-                net.places.add(corr_places[act])
-            add_arc_from_to(corr_places[act], ht_sink, net)
+        for index2, act in enumerate(end_activities):
+            ht_sink = PetriNet.Transition("htsi_" + perspective + "_" + str(index2))
+            net.transitions.add(ht_sink)
+            add_arc_from_to(ht_sink, sink, net)
+
+            #pact = perspective + "@@" + act
+            pact = act
+            if pact not in corr_places:
+                corr_places[pact] = PetriNet.Place("p_"+pact)
+                net.places.add(corr_places[pact])
+            pact2 = "end_" + perspective + "@@" + act
+            corr_places[pact2] = PetriNet.Place("p_" + pact2)
+            net.places.add(corr_places[pact2])
+
+            et = PetriNet.Transition("et_"+pact+"_"+perspective, act)
+            net.transitions.add(et)
+
+            add_arc_from_to(corr_places[pact], et, net)
+            add_arc_from_to(et, corr_places[pact2], net)
+            add_arc_from_to(corr_places[pact2], ht_sink, net)
 
         for i1, n1 in enumerate(nodes):
-            if n1 not in corr_places:
-                corr_places[n1] = PetriNet.Place("p1_"+n1)
-                net.places.add(corr_places[n1])
+            could_be_added = False
             for i2, n2n in enumerate(nodes[n1].output_connections):
                 n2 = n2n.node_name
-                if n2 not in corr_places:
-                    corr_places[n2] = PetriNet.Place("p2_"+n2)
-                    net.places.add(corr_places[n2])
-                trans = PetriNet.Transition("tr_"+str(n1)+"_"+str(n2), n1)
-                net.transitions.add(trans)
-                if (str(n1), "tr_"+str(n1)+"_"+str(n2)) not in added_arcs:
-                    add_arc_from_to(corr_places[n1], trans, net)
-                    added_arcs.add((str(n1), "tr_"+str(n1)+"_"+str(n2)))
-                if ("tr_"+str(n1)+"_"+str(n2), str(n2)) not in added_arcs:
-                    add_arc_from_to(trans, corr_places[n2], net)
-                    added_arcs.add(("tr_"+str(n1)+"_"+str(n2), str(n2)))
+                if not n1 == n2:
+                    could_be_added = True
+                    break
+            if could_be_added:
+                #n1p = perspective + "@@" + n1
+                n1p = n1
+                if n1p not in corr_places:
+                    corr_places[n1p] = PetriNet.Place("p1_"+n1p)
+                    net.places.add(corr_places[n1p])
+                for i2, n2n in enumerate(nodes[n1].output_connections):
+                    n2 = n2n.node_name
+                    #n2p = perspective + "@@" + n2
+                    n2p = n2
+                    if n2p not in corr_places:
+                        corr_places[n2p] = PetriNet.Place("p2_"+n2p)
+                        net.places.add(corr_places[n2p])
+                    trans = PetriNet.Transition("tr_"+str(n1)+"_"+str(n2), n1)
+                    net.transitions.add(trans)
+
+                    if (str(n1), "tr_"+str(n1)+"_"+str(n2)) not in added_arcs:
+                        add_arc_from_to(corr_places[n1p], trans, net)
+                        added_arcs.add((str(n1), "tr_"+str(n1)+"_"+str(n2)))
+                    if ("tr_"+str(n1)+"_"+str(n2), str(n2)) not in added_arcs:
+                        add_arc_from_to(trans, corr_places[n2p], net)
+                        added_arcs.add(("tr_"+str(n1)+"_"+str(n2), str(n2)))
 
         for place in net.places:
             if len(place.in_arcs) < 1 and not place.name == "master_source":
