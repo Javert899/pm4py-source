53a0854a4b57a3ec0f6e21440c08d57f253b79d0
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-02 13:31:31 +0200

    Refactoring

--- pm4py/objects/log/exporter/parquet/versions/pyarrow.py
+++ pm4py/objects/log/exporter/parquet/versions/pyarrow.py
@@ -35,16 +35,3 @@ def apply(df, path, parameters=None):
         pq.write_to_dataset(df, path, compression=compression, partition_cols=partition_cols)
     else:
         pq.write_table(df, path, compression=compression)
-
-
-def export_to_dataset(df, path, parameters=None):
-    if parameters is None:
-        parameters = {}
-
-    compression = parameters["compression"] if "compression" in parameters else "snappy"
-    partition_cols = parameters["partition_cols"] if "partition_cols" in parameters else None
-
-    df.columns = [x.replace(":", "AAA") for x in df.columns]
-
-    df = pa.Table.from_pandas(df)
-    pq.write_to_dataset(df, root_path=path, compression=compression, partition_cols=partition_cols)
