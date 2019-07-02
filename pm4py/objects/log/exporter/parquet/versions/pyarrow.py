c83bd6714c206e5c4378b77a3de41c7f86e7cb3a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-02 13:26:34 +0200

    Added exporting to dataset after partitioning

--- pm4py/objects/log/exporter/parquet/versions/pyarrow.py
+++ pm4py/objects/log/exporter/parquet/versions/pyarrow.py
@@ -1,6 +1,7 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
-
+import os
+import shutil
 
 def apply(df, path, parameters=None):
     """
@@ -19,7 +20,30 @@ def apply(df, path, parameters=None):
         parameters = {}
 
     compression = parameters["compression"] if "compression" in parameters else "snappy"
+    partition_cols = parameters["partition_cols"] if "partition_cols" in parameters else None
 
     df.columns = [x.replace(":", "AAA") for x in df.columns]
+    if partition_cols is not None:
+        partition_cols = [x.replace(":", "AAA") for x in partition_cols]
     df = pa.Table.from_pandas(df)
-    pq.write_table(df, path, compression=compression)
+
+    if partition_cols is not None:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+        pq.write_to_dataset(df, path, compression=compression, partition_cols=partition_cols)
+    else:
+        pq.write_table(df, path, compression=compression)
+
+
+def export_to_dataset(df, path, parameters=None):
+    if parameters is None:
+        parameters = {}
+
+    compression = parameters["compression"] if "compression" in parameters else "snappy"
+    partition_cols = parameters["partition_cols"] if "partition_cols" in parameters else None
+
+    df.columns = [x.replace(":", "AAA") for x in df.columns]
+
+    df = pa.Table.from_pandas(df)
+    pq.write_to_dataset(df, root_path=path, compression=compression, partition_cols=partition_cols)
