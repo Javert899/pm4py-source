6a1d20087b541695b992256dc4e61b79e808c82a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-30 16:49:16 +0200

    Intermediate commit

--- pm4py/visualization/mvp/clustered_pcl_dfg/visualizer.py
+++ pm4py/visualization/mvp/clustered_pcl_dfg/visualizer.py
@@ -1,4 +1,62 @@
 from graphviz import Digraph
+from pm4py.visualization.common import gview
+from pm4py.visualization.common import save as gsave
+import uuid
+import tempfile
+
 
 def apply(input_object, parameters=None):
-    pass
+    if parameters is None:
+        parameters = {}
+
+    image_format = "png"
+    if "format" in parameters:
+        image_format = parameters["format"]
+
+
+    filename = tempfile.NamedTemporaryFile(suffix='.gv')
+    g = Digraph("", filename=filename.name, engine='dot', graph_attr={'bgcolor': 'transparent'})
+
+    nodes = {}
+
+    for p in input_object:
+        nodes[p] = {}
+        with g.subgraph(name='cluster_'+p) as c:
+            c.attr(style='filled')
+            c.attr(color='lightgrey')
+            c.node_attr.update(style='filled', color='white')
+            for a in input_object[p]["activities_count"]:
+                nodes[p][a] = c.node(str(uuid.uuid4()), a)
+            c.attr(label='class: '+p)
+
+    g.attr(overlap='false')
+    g.attr(fontsize='11')
+
+    g.format = image_format
+
+    return g
+
+def save(gviz, output_file_path):
+    """
+    Save the diagram
+
+    Parameters
+    -----------
+    gviz
+        GraphViz diagram
+    output_file_path
+        Path where the GraphViz output should be saved
+    """
+    gsave.save(gviz, output_file_path)
+
+
+def view(gviz):
+    """
+    View the diagram
+
+    Parameters
+    -----------
+    gviz
+        GraphViz diagram
+    """
+    return gview.view(gviz)
