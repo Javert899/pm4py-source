5b783890577eda19095950939327e8f3af5116b9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-30 12:55:11 +0200

    Get first and last for object

---
+++ pm4py/algo/mvp/crd/get_last_for_object.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def get_last(df, parameters=None):
+    columns = [x for x in df.columns if not x.startswith("event_")]
+
+    all_dfs = []
+
+    for c in columns:
+        red_df = df.groupby(c).last()
+
+        all_dfs.append(red_df)
+
+    return pd.concat(all_dfs).sort_values(['event_timestamp', 'event_id'])
