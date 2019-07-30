e2b5370b2850aa090cdaee49a633c6d22ae80c74
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-30 16:34:18 +0200

    Intermediate commit

--- pm4py/algo/mvp/general_pcl_dfg/get_object.py
+++ pm4py/algo/mvp/general_pcl_dfg/get_object.py
@@ -3,11 +3,19 @@ from pm4py.algo.filtering.pandas.attributes import attributes_filter
 from pm4py.algo.filtering.pandas.start_activities import start_activities_filter
 from pm4py.algo.filtering.pandas.end_activities import end_activities_filter
 from pm4py.util import constants
+from pm4py.objects.heuristics_net import defaults
+
+MIN_ACT_COUNT = "min_act_count"
+MIN_DFG_OCCURRENCES = "min_dfg_occurrences"
 
 
 def apply(df, parameters=None):
     if parameters is None:
         parameters = {}
+
+    min_act_count = parameters[MIN_ACT_COUNT] if MIN_ACT_COUNT in parameters else 5
+    min_dfg_occurrences = parameters[
+        MIN_DFG_OCCURRENCES] if MIN_DFG_OCCURRENCES in parameters else 3
 
     ret = {}
 
@@ -17,18 +25,14 @@ def apply(df, parameters=None):
 
         if not col.startswith("event_"):
             red_df = df[["event_id", "event_activity", "event_timestamp", col]]
-            #print(len(red_df))
-            #print(red_df)
             red_df = red_df.groupby(["event_id", "event_activity", col]).first().reset_index()
-            #print(len(red_df))
-            #print(red_df)
-            print(red_df)
 
             dfg = df_statistics.get_dfg_graph(red_df, sort_timestamp_along_case_id=True, sort_caseid_required=True,
                                               activity_key="event_activity", timestamp_key="event_timestamp",
                                               case_id_glue=col)
 
             activities_count = attributes_filter.get_attribute_values(red_df, "event_activity")
+            activities_count = {x: y for x, y in activities_count.items() if y >= min_act_count}
 
             start_activities = start_activities_filter.get_start_activities(red_df, parameters={
                 constants.PARAMETER_CONSTANT_ACTIVITY_KEY: "event_activity",
@@ -37,6 +41,9 @@ def apply(df, parameters=None):
             end_activities = end_activities_filter.get_end_activities(red_df, parameters={
                 constants.PARAMETER_CONSTANT_ACTIVITY_KEY: "event_activity",
                 constants.PARAMETER_CONSTANT_CASEID_KEY: "event_timestamp"})
+
+            dfg = {x: y for x, y in dfg.items() if
+                   x[0] in activities_count and x[1] in activities_count and y >= min_dfg_occurrences}
 
             if len(dfg) > 0:
                 ret[col] = {}
