d737232ec7073990dd7244dd6db5b6107a43b988
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-01 16:33:01 +0200

    New

--- pm4py/algo/mvp/crd/mine_inside_relationship.py
+++ pm4py/algo/mvp/crd/mine_inside_relationship.py
@@ -47,8 +47,6 @@ def apply(df, parameters=None):
 
                         rel_count = dict(joined_df.groupby(["event_activity", "event_activity_2"]).size())
 
-                        print(col, c2, rel_count)
-
                         for couple in rel_count:
                             act1 = couple[0]
                             act2 = couple[1]
@@ -71,7 +69,7 @@ def apply(df, parameters=None):
                     if temp_ports[col][c2][act2]:
                         all_keys = list(temp_ports[col][c2][act2].keys())
                         summ = np.sum([x[0] for x in temp_ports[col][c2][act2].values()])
-                        if summ == temp_ports[col][c2][act2][all_keys[0]][2] or True:
+                        if summ == temp_ports[col][c2][act2][all_keys[0]][2]:
                             if not col in ports:
                                 ports[col] = {}
                             if not c2 in ports[col]:
