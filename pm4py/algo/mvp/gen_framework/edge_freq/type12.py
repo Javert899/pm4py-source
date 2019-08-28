474a01d4f977a2a06f71b93c95c99cf20e8b1238
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-28 14:02:07 +0200

    Added second type of arc frequency

---
+++ pm4py/algo/mvp/gen_framework/edge_freq/type12.py
@@ -0,0 +1,18 @@
+def apply(rel_ev, rel_act, parameters=None):
+    if parameters is None:
+        parameters = {}
+
+    ret = {}
+
+    for persp in rel_ev:
+        df = rel_ev[persp]
+        df = df[df["event_activity_merge"].isin(rel_act[persp])]
+        print(df)
+        df = df.groupby(["event_id", "event_id_2"]).first()
+        #print("BBB", len(df))
+
+        r = df.groupby("event_activity_merge").size().to_dict()
+
+        ret[persp] = r
+
+    return ret
