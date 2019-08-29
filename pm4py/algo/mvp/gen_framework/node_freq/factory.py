920dbc94e916ecad0f047fcd44f8c0f8f2e69c76
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 13:11:53 +0200

    Added freq type22 and type23

--- pm4py/algo/mvp/gen_framework/node_freq/factory.py
+++ pm4py/algo/mvp/gen_framework/node_freq/factory.py
@@ -1,9 +1,11 @@
-from pm4py.algo.mvp.gen_framework.node_freq import type1, type21
+from pm4py.algo.mvp.gen_framework.node_freq import type1, type21, type22, type23
 
 TYPE1 = "type1"
 TYPE21 = "type21"
+TYPE22 = "type22"
+TYPE23 = "type23"
 
-VERSIONS = {TYPE1: type1.apply, TYPE21: type21.apply}
+VERSIONS = {TYPE1: type1.apply, TYPE21: type21.apply, TYPE22: type22.apply, TYPE23: type23.apply}
 
 
 def apply(df, model, rel_ev, rel_act, variant=TYPE1, parameters=None):
