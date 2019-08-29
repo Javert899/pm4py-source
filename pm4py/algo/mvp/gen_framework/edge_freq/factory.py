4d3a690f86e508d4845f060375fef8d1e10ae54a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 10:08:31 +0200

    Added type222

--- pm4py/algo/mvp/gen_framework/edge_freq/factory.py
+++ pm4py/algo/mvp/gen_framework/edge_freq/factory.py
@@ -1,4 +1,4 @@
-from pm4py.algo.mvp.gen_framework.edge_freq import type11, type12, type13, type211, type212, type221
+from pm4py.algo.mvp.gen_framework.edge_freq import type11, type12, type13, type211, type212, type221, type222
 
 TYPE11 = "type11"
 TYPE12 = "type12"
@@ -6,9 +6,10 @@ TYPE13 = "type13"
 TYPE211 = "type211"
 TYPE212 = "type212"
 TYPE221 = "type221"
+TYPE222 = "type222"
 
 VERSIONS = {TYPE11: type11.apply, TYPE12: type12.apply, TYPE13: type13.apply, TYPE211: type211.apply,
-            TYPE212: type212.apply, TYPE221: type221.apply}
+            TYPE212: type212.apply, TYPE221: type221.apply, TYPE222: type222.apply}
 
 
 def apply(df, model, rel_ev, rel_act, variant=TYPE11, parameters=None):
