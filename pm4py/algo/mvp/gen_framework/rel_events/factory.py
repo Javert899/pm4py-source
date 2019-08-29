bdc7b2efaffc6bc99dd8a0702ca19c34e0a62cb8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 14:28:46 +0200

    Added link pattern

--- pm4py/algo/mvp/gen_framework/rel_events/factory.py
+++ pm4py/algo/mvp/gen_framework/rel_events/factory.py
@@ -1,12 +1,14 @@
 from pm4py.algo.mvp.gen_framework.rel_events import rel_dfg
 from pm4py.algo.mvp.gen_framework.rel_events import being_produced
 from pm4py.algo.mvp.gen_framework.rel_events import existence
+from pm4py.algo.mvp.gen_framework.rel_events import link
 
 REL_DFG = "rel_dfg"
 BEING_PRODUCED = "being_produced"
 EXISTENCE = "existence"
+LINK = "link"
 
-VERSIONS = {REL_DFG: rel_dfg.apply, BEING_PRODUCED: being_produced.apply, EXISTENCE: existence.apply}
+VERSIONS = {REL_DFG: rel_dfg.apply, BEING_PRODUCED: being_produced.apply, EXISTENCE: existence.apply, LINK: link.apply}
 
 
 def apply(df, model, variant=REL_DFG, parameters=None):
