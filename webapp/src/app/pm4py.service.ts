8555b04ad1dc1de1382a4b16da33fac4988324f1
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 07:44:24 +0200

    Service

--- webapp/src/app/pm4py.service.ts
+++ webapp/src/app/pm4py.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Pm4pyService {
+  webservicePath: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.webservicePath = window.location.protocol+'//' + window.location.hostname + ':5000/';
+  }
+
+  getProcessSchema(parameters: HttpParams) {
+    var completeUrl : string = this.webservicePath + "getProcessSchema";
+
+    return this.http.get(completeUrl,{params: parameters});
+  }
 }
--- webapp/src/app/pmodel/pmodel.component.ts
+++ webapp/src/app/pmodel/pmodel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Pm4pyService} from '../pm4py.service';
+import { graphviz } from 'd3-graphviz';
+import {HttpParams} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-pmodel',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pmodel.component.scss']
 })
 export class PmodelComponent implements OnInit {
+  public gviz : string;
+  public model : string;
 
-  constructor() { }
+  constructor(public pm4pyService : Pm4pyService) {
+    this.gviz = "";
+    this.model = "";
+
+    this.getProcessSchema();
+  }
 
   ngOnInit() {
+  }
+
+  getProcessSchema() {
+    let httpParameters : HttpParams = new HttpParams();
+
+    this.pm4pyService.getProcessSchema(httpParameters).subscribe(data => {
+      let pm4pyJson = data as JSON;
+      this.gviz = pm4pyJson['gviz'];
+      this.model = pm4pyJson['model'];
+      if (this.gviz == null || typeof(this.gviz) == "undefined") {
+        this.gviz = "";
+      }
+      if (this.gviz.length > 0) {
+        this.gviz = atob(this.gviz);
+        this.model = atob(this.model);
+
+        console.log(this.gviz);
+        console.log(this.model);
+
+        let targetInnerWidth = window.innerWidth * 0.75;
+        let targetInnerHeight = window.innerHeight * 0.93;
+
+        let targetWidth = window.innerWidth * 0.77;
+        let targetHeight = window.innerHeight * 0.95;
+      }
+
+    });
   }
 
 }
