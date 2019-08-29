759cbafb885ed561d836583e54d79bbe7ec1f586
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 07:25:39 +0200

    Intermediate commit

---
+++ webapp/src/app/pmodel/pmodel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pmodel',
+  templateUrl: './pmodel.component.html',
+  styleUrls: ['./pmodel.component.scss']
+})
+export class PmodelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
