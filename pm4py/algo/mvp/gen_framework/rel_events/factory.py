56a8367dbf6ed5b60f36a6aa5dce6fe2af9fc9b9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-29 14:19:37 +0200

    Added existence events relationship

--- pm4py/algo/mvp/gen_framework/rel_events/factory.py
+++ pm4py/algo/mvp/gen_framework/rel_events/factory.py
@@ -1,10 +1,12 @@
 from pm4py.algo.mvp.gen_framework.rel_events import rel_dfg
 from pm4py.algo.mvp.gen_framework.rel_events import being_produced
+from pm4py.algo.mvp.gen_framework.rel_events import existence
 
 REL_DFG = "rel_dfg"
 BEING_PRODUCED = "being_produced"
+EXISTENCE = "existence"
 
-VERSIONS = {REL_DFG: rel_dfg.apply, BEING_PRODUCED: being_produced.apply}
+VERSIONS = {REL_DFG: rel_dfg.apply, BEING_PRODUCED: being_produced.apply, EXISTENCE: existence.apply}
 
 
 def apply(df, model, variant=REL_DFG, parameters=None):
